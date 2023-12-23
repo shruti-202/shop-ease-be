652970a13289573b010dadb68657d382e8bbc4e1
Shruti <shrutigawande1910@gmail.com>
2023-12-23 14:23:55 +0530

    feat: added validations

--- constants/Validator.js
+++ constants/Validator.js
@@ -8,10 +8,14 @@ const emailValidator = (email) => {
   return emailRegex.test(email);
 };
 
+const userNameValidator = (username) => {
+  const usernameRegex = /^[A-Za-z][A-Za-z0-9_]{7,29}$/;
+  return usernameRegex.test(username);
+}
 const passwordValidator = (password) => {
   const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     return passwordRegex.test(password)
 }
 
 
-module.exports = { nameValidator, emailValidator , passwordValidator };
+module.exports = { nameValidator, emailValidator , userNameValidator, passwordValidator };
